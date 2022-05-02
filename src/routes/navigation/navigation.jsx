a6e377a9a010f408f806e6d44481eae0fff454a2
Michael Pretorius <66328104+Michael-Envel@users.noreply.github.com>
2022-05-02 12:23:49 +0200

    adding cart and shop

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -5,9 +5,13 @@ import './navigation.scss';
 import { UserContext } from '../../contexts/user';
 import { signOutUser } from '../../utils/firebase/firebase';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import CartIcon from '../../components/cart-icon/cart-icon';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown';
+import { CartContext } from '../../contexts/cart';
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
+	const { isCartOpen } = useContext(CartContext);
 
 	return (
 		<>
@@ -28,7 +32,9 @@ const Navigation = () => {
 							SIGN IN
 						</Link>
 					)}
+					<CartIcon />
 				</div>
+				{isCartOpen && <CartDropdown />}
 			</div>
 			<Outlet />
 		</>
